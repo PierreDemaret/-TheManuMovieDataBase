5a503653350cdcb7918f1721bd5583ec2097af04
Pierre <demaret.pierre.pro@gmail.com>
2023-07-05 14:43:50 +0200

    fin The movie database

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+
+// const div = document.getElementById('card')
+// div.classList.add('col-3')
+// const txtContent = document.createElement('div')
+// txtContent.appendChild(title)
+// txtContent.appendChild(overview)
+// txtContent.appendChild(vote)
+// document.body.appendChild(txtContent)
+
+
+
+// let test = fetch('../../movies.json')
+//   .then((res) => res.json())
+//   .then((data) => {
+//     data.results.forEach(element => {
+//         let img = element.poster_path
+//         let title = element.original_title
+//         let overview = element.overview
+//         let vote = element.vote_average
+        
+//         let imgEl = document.createElement('img')
+//         imgEl.classList.add('col-5')
+//         imgEl.src = img
+//         div.appendChild(imgEl)
+
+//         let titleEl = document.createElement('h3')
+//         titleEl.classList.add('col-2')
+//         titleEl.textContent = title
+//         div.appendChild(titleEl)
+
+//         let overviewEl = document.createElement('p')
+//         overviewEl.classList.add('col-3')
+//         overviewEl.textContent = overview
+//         div.appendChild(overviewEl)
+
+//         let voteEl = document.createElement('p')
+//         voteEl.textContent = vote
+//         div.appendChild(voteEl)
+        
+        
+//     });
+//   });
+
+
+
+
+
